a5a94b21c7d0d5f6beba5413a8a946cbc0b05f43
Achilles45 <achillesusuoyibo7@gmail.com>
2020-05-26 22:39:42 -0700

    Changed project name

--- src/firebase/init.js
+++ src/firebase/init.js
@@ -2,14 +2,14 @@ import firebase from 'firebase';
 import firestore from 'firebase/firestore';
 
 var firebaseConfig = {
-  apiKey: "AIzaSyALLluTQ_vzKEqyJUrQE_I-AyWFYxaiap8",
-  authDomain: "fidelityapp-5fa95.firebaseapp.com",
-  databaseURL: "https://fidelityapp-5fa95.firebaseio.com",
-  projectId: "fidelityapp-5fa95",
-  storageBucket: "fidelityapp-5fa95.appspot.com",
-  messagingSenderId: "601715198667",
-  appId: "1:601715198667:web:86f0ebabd2ce145df0a94e",
-  measurementId: "G-PFTSWTFWF0"
+  apiKey: "AIzaSyBdlp3iY1HkD1yfVsKHtcEvK_jQ037k0Ow",
+  authDomain: "fidelity-income.firebaseapp.com",
+  databaseURL: "https://fidelity-income.firebaseio.com",
+  projectId: "fidelity-income",
+  storageBucket: "fidelity-income.appspot.com",
+  messagingSenderId: "839125393619",
+  appId: "1:839125393619:web:d0ed0bf847691bc0f23426",
+  measurementId: "G-86RZQHF6PD"
 };
   // Initialize Firebase
   const firebaseApp = firebase.initializeApp(firebaseConfig);
