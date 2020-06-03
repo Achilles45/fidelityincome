6134807c077018f73564a60d2faf9aa328e85fe3
Achilles45 <achillesusuoyibo7@gmail.com>
2020-06-02 20:33:57 -0700

    Changed the email and firebase dashboard

--- src/firebase/init.js
+++ src/firebase/init.js
@@ -2,14 +2,14 @@ import firebase from 'firebase';
 import firestore from 'firebase/firestore';
 
 var firebaseConfig = {
-  apiKey: "AIzaSyBdlp3iY1HkD1yfVsKHtcEvK_jQ037k0Ow",
-  authDomain: "fidelity-income.firebaseapp.com",
-  databaseURL: "https://fidelity-income.firebaseio.com",
-  projectId: "fidelity-income",
-  storageBucket: "fidelity-income.appspot.com",
-  messagingSenderId: "839125393619",
-  appId: "1:839125393619:web:d0ed0bf847691bc0f23426",
-  measurementId: "G-86RZQHF6PD"
+  apiKey: "AIzaSyAFj_-OWAvWL9XxUskcsVYdIGFmYA0yE0I",
+  authDomain: "fidelity-income-fcd95.firebaseapp.com",
+  databaseURL: "https://fidelity-income-fcd95.firebaseio.com",
+  projectId: "fidelity-income-fcd95",
+  storageBucket: "fidelity-income-fcd95.appspot.com",
+  messagingSenderId: "214807404559",
+  appId: "1:214807404559:web:070b2fd636c1f747b4c37a",
+  measurementId: "G-4VN6LS9KHC"
 };
   // Initialize Firebase
   const firebaseApp = firebase.initializeApp(firebaseConfig);
